ed4b5b4593bd8bd745344e86f78b0684ecbd8aae
VinnyMendes <vinypotter.vm@gmail.com>
2021-11-18 11:10:42 -0300

    criando banana project

---
+++ src/main/java/dev/vinny/infraJenkins/InfraJenkinsApplication.java
@@ -0,0 +1,13 @@
+package dev.vinny.infraJenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfraJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfraJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/vinny/infraJenkins/InfraJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vinny.infraJenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfraJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
